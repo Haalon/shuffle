e9fe2c1bc5c6a9d1769b6afc9644d0d1c5bb543b
Haalon <haalon@yandex.com>
2021-08-14 19:12:22 +0300

    Add cors

--- back/punchline_shuffle/punchline_shuffle/settings.py
+++ back/punchline_shuffle/punchline_shuffle/settings.py
@@ -39,7 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'jokes',
     'rest_framework',
+    'corsheaders',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+# CORS_ORIGIN_WHITELIST = (
+#        'localhost:3000',
+# )
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -49,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'punchline_shuffle.urls'
