148ccc261e0f3b23e55e0fa626d5a9a38c8f5434
Haalon <haalon@yandex.com>
2022-08-26 14:44:01 +0300

    Transition to mysql db in container

--- back/punchline_shuffle/punchline_shuffle/settings.py
+++ back/punchline_shuffle/punchline_shuffle/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'punchline_shuffle.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'shuffle',
+        'USER': 'root',
+        'PASSWORD': 'secret',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
