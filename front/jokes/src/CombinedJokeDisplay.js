10aadd92d9e7288098b9f206ab25ff0ee4307e23
Haalon <haalon@yandex.com>
2022-08-29 20:50:54 +0300

    Update frontend UI

--- front/jokes/src/CombinedJokeDisplay.js
+++ front/jokes/src/CombinedJokeDisplay.js
@@ -12,13 +12,14 @@ class CombinedJokeDisplay extends Component {
         this.state = {
             combinedJoke: null,
             lang: 'RU',
+            display: 'combined'
         };
     }
 
     generateNewCombinedJoke() {
         jokeAPI.generateCombinedJoke(this.state.lang)
         .then(result =>
-            this.setState({ combinedJoke: result})
+            this.setState({ combinedJoke: result, display: 'combined'})
         );
     }
 
@@ -29,10 +30,23 @@ class CombinedJokeDisplay extends Component {
     getText() {
         if (!this.state.combinedJoke) return (<span></span>);
 
-        const destText = this.state.combinedJoke.destination.body;
-        const punchline = this.state.combinedJoke.corrected_punchline;
 
-        const combText = destText.replace(/\{.*?\}/, `|&${punchline}|`);
+        let text, punchline;
+        switch (this.state.display) {
+            case 'source': 
+                text = this.state.combinedJoke.source.body;
+                punchline = this.state.combinedJoke.source.punchline;
+                break;
+            case 'destination':
+                text = this.state.combinedJoke.destination.body;
+                punchline = this.state.combinedJoke.destination.punchline;
+                break;
+            default: 
+                text = this.state.combinedJoke.destination.body;
+                punchline = this.state.combinedJoke.corrected_punchline;
+                break;
+        }
+        const combText = text.replace(/\{.*?\}/, `|&${punchline}|`);
         const parts = combText.split('|');
         const tagParts = parts.map((txt, i) => {
             if (txt[0] == '&') return (
@@ -50,6 +64,11 @@ class CombinedJokeDisplay extends Component {
         return tagParts;
     }
 
+    getButtionHighlight(display) {
+        if (this.state.display === display) return 'button-highlight';
+        
+        return '';
+    }
 
     render() {
         if (!this.state.combinedJoke) return <div>Not loaded yet</div>;
@@ -57,18 +76,26 @@ class CombinedJokeDisplay extends Component {
         console.log(this.state.combinedJoke.body);
         return (
             <div className="container">
+
+                <div className="button-section button-section--top">
+                    <div className={`button button_border_left ${this.getButtionHighlight('source')}`} onClick={e => this.setState({display: 'source'})}>
+                        Source
+                    </div>
+                    <div className={`button button_border_center ${this.getButtionHighlight('combined')}`} onClick={e => this.setState({display: 'combined'})}>
+                        Combined
+                    </div>
+                    <div className={`button button_border_right ${this.getButtionHighlight('destination')}`} onClick={e => this.setState({display: 'destination'})}>
+                        Destination
+                    </div>
+                </div>
+
                 <div className="display">
                     {this.getText()}
                 </div>
-                <div className="button-section">
-                    <div className="button button_border_left">
-                        Show origin
-                    </div>
-                    <div className="button button_border_center" onClick={e => this.generateNewCombinedJoke()}>
+
+                <div className="button-section button-section--bottom">
+                    <div className="button button_border_single" onClick={e => this.generateNewCombinedJoke()}>
                         Generate new Joke
-                    </div>
-                    <div className="button button_border_right">
-                        Show destination
                     </div>
                 </div>
             </div>
